f5b3d3f04f3fb6b86b11d83e2e164634708eaaea
datbh <bdat1606@gmail.com>
2023-08-18 00:38:21 +0700

    Initial commit

---
+++ todo-mgmt-FE/src/components/ListTodosComponent.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+
+const ListTodosComponent = () => {
+
+    const [todos, setTodos] = useState([]);
+
+    return (<div>
+
+        </div>);
+};
+
+export default ListTodosComponent;
