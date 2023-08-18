a15cb21507e6f073dc032750aaf434f18ce041cf
datbh06 <bdat1606@gmail.com>
2023-08-18 08:04:57 +0700

    feats: Add Todo Form Handling

--- todo-mgmt-FE/src/components/TodoComponent.jsx
+++ todo-mgmt-FE/src/components/TodoComponent.jsx
@@ -1,15 +1,74 @@
 import React, {useState} from 'react';
+import {useNavigate, useParams} from "react-router-dom";
+import {createTodo} from "../services/TodoService.js";
 
 const TodoComponent = () => {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [completed, setCompleted] = useState('');
     const [todos, setTodos] = useState([]);
 
+    const navigator = useNavigate();
 
+    const {id} = useParams();
 
-    return (
-        <div>
+    function pageTitle() {
+        if (id) {
+            return <h2 className='text-center'>Update Task</h2>;
+        } else {
+            return <h2 className='text-center'>Add Task</h2>;
+        }
+    }
 
+    function saveOrUpdateTodo(e) {
+
+        e.preventDefault();
+
+        const todo = {title, description, completed};
+        console.log(todo);
+
+        createTodo(todo).then((res) => {
+            console.log(res.data);
+            navigator('/todos');
+        }).catch((err) => {
+            console.error(err);
+        });
+
+    }
+
+    return (<div className='container'>
+        <br/>
+        <br/>
+        <div className='row'>
+            <div className='card col-md-6 offset-md-3 offset-md-3'>
+                {pageTitle()}
+                <div className="card-body">
+                    <form>
+                        <div className="form-group mb-2">
+                            <label className='form-label'>Task:</label>
+                            <input type="text" className='form-control' value={title} placeholder="Enter task"
+                                   onChange={(e) => setTitle(e.target.value)}/>
+                        </div>
+                        <div className="form-group mb-2">
+                            <label className='form-label'>Description:</label>
+                            <input type="text" className='form-control' value={description}
+                                   placeholder="Enter description"
+                                   onChange={(e) => setDescription(e.target.value)}/>
+                        </div>
+                        <div className="form-group mb-2">
+                            <label className='form-label'>Status:</label>
+                            <select className='form-control' value={completed}
+                                    onChange={(e) => setCompleted(e.target.value)}>
+                                <option value=""></option>
+                                <option value="true">Completed</option>
+                                <option value="false">Pending</option>
+                            </select>
+                        </div>
+                        <button className="btn btn-success" onClick={saveOrUpdateTodo}>Save</button>
+                    </form>
+                </div>
+            </div>
         </div>
-    );
+    </div>)
 };
-
 export default TodoComponent;
--- todo-mgmt-FE/src/services/TodoService.js
+++ todo-mgmt-FE/src/services/TodoService.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
 const TODO_API_BASE_URL = "http://localhost:8080/api/v1/todos";
+const TODO_API_BASE_URL_CREATE = "http://localhost:8080/api/v1/todos/create";
 
 export const getAllTodos = () => axios.get(TODO_API_BASE_URL);
+
+export const createTodo = (todo) => axios.post(TODO_API_BASE_URL_CREATE, todo);
 
