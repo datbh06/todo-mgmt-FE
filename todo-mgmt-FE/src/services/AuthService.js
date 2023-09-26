8105c9a0bbc5538123775cfbfb3acde4cd671c07
datbh06 <bdat1606@gmail.com>
2023-09-26 17:58:33 +0700

    refactor: test deployment

--- todo-mgmt-FE/src/services/AuthService.js
+++ todo-mgmt-FE/src/services/AuthService.js
@@ -1,34 +1,26 @@
 import axios from "axios";
 
-const AUTH_API_BASE_URL = "http://localhost:8080/api/v1/auth";
+const AUTH_API_BASE_URL = "https://todo-spring-env.eba-3atqpz9u.ap-southeast-2.elasticbeanstalk.com/api/v1/auth";
 
 export const registerAPICall = (newUser) => axios.post(AUTH_API_BASE_URL + "/register", newUser);
-
 export const loginAPICall = (usernameOrEmail, password) => axios.post(AUTH_API_BASE_URL + "/login", {
     usernameOrEmail, password
 });
-
 export const storeToken = (token) => localStorage.setItem("token", token);
-
 export const getToken = () => localStorage.getItem("token");
-
 export const savedLoggedInUser = (username, role) => {
     sessionStorage.setItem("authenticatedUser", username);
     sessionStorage.setItem("role", role);
-
 }
-
 export const isUserLoggedIn = () => {
     const username = sessionStorage.getItem("authenticatedUser");
     return username != null;
 }
 export const getLoggedInUser = () => sessionStorage.getItem("authenticatedUser");
-
 export const logout = () => {
     localStorage.clear();
     sessionStorage.clear();
 }
-
 export const isAdminUser = () => {
     let role = sessionStorage.getItem("role");
     return role !== null && role === "ROLE_ADMIN";
--- todo-mgmt-FE/src/services/TodoService.js
+++ todo-mgmt-FE/src/services/TodoService.js
@@ -9,23 +9,15 @@ axios.interceptors.request.use(function (config) {
     return Promise.reject(error);
 });
 
-
-const TODO_API_BASE_URL = "http://localhost:8080/api/v1/todos";
-const TODO_API_BASE_URL_CREATE = "http://localhost:8080/api/v1/todos/create";
-const TODO_API_BASE_URL_UPDATE = "http://localhost:8080/api/v1/todos/update";
-const TODO_API_BASE_URL_DELETE = "http://localhost:8080/api/v1/todos/delete";
-
+const TODO_API_BASE_URL = "https://todo-spring-env.eba-3atqpz9u.ap-southeast-2.elasticbeanstalk.com/api/v1/todos";
+const TODO_API_BASE_URL_CREATE = "https://todo-spring-env.eba-3atqpz9u.ap-southeast-2.elasticbeanstalk.com/api/v1/todos/create";
+const TODO_API_BASE_URL_UPDATE = "https://todo-spring-env.eba-3atqpz9u.ap-southeast-2.elasticbeanstalk.com/api/v1/todos/update";
+const TODO_API_BASE_URL_DELETE = "https://todo-spring-env.eba-3atqpz9u.ap-southeast-2.elasticbeanstalk.com/api/v1/todos/delete";
 
 export const getAllTodos = () => axios.get(TODO_API_BASE_URL);
-
 export const createTodo = (todo) => axios.post(TODO_API_BASE_URL_CREATE, todo);
-
 export const getTodoById = (todoId) => axios.get(TODO_API_BASE_URL + "/" + todoId);
-
 export const updateTodo = (todoId, todo) => axios.put(TODO_API_BASE_URL_UPDATE + "/" + todoId, todo);
-
 export const deleteTodo = (todoId) => axios.delete(TODO_API_BASE_URL_DELETE + "/" + todoId);
-
 export const completedTodo = (todoId) => axios.patch(TODO_API_BASE_URL + "/" + todoId + "/completed");
-
 export const uncompletedTodo = (todoId) => axios.patch(TODO_API_BASE_URL + "/" + todoId + "/uncompleted");
